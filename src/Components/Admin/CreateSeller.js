fe053ea9b8d8de5bd51647eb2c57348bd2c46d37
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2022-08-15 20:52:14 +0600

    REACT

---
+++ src/Components/Admin/CreateSeller.js
@@ -0,0 +1,86 @@
+import "./CSS/adminDashboard.css";
+import LeftBar from './Bar/LeftBar';
+//import {Link} from 'react-router-dom';
+import {useState} from 'react';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+
+const CreateSeller =()=>{
+    const[s_name,setName] = useState("");
+    const[s_phn,setPhn] = useState("");
+    const[s_mail,setMail] = useState("");
+    const[s_pass,setPass] = useState("");
+    const[s_add,setAdd] = useState("");
+    const navigate = useNavigate();
+    const[msg,setMsg] = useState("");
+    const[err,setErr] = useState("");
+
+
+    const handleForm=(event)=>{
+        event.preventDefault();
+        setName('');
+        setPhn('');
+        setMail('');
+        setPass('');
+        setAdd('');
+        setMsg('');
+        setErr('');
+
+        var data = {s_name:s_name,s_phn:s_phn, s_mail:s_mail,s_pass:s_pass,s_add:s_add};
+        axios.post("http://localhost:8000/api/admin/files/createSeller",data).then((rsp)=>{
+            navigate('/Admin/Seller');
+            setMsg(rsp.data.msg);
+        },(err)=>{
+            if(err.response.status==422) //for data validation
+            {
+                setErr(err.response.data);
+            }else{
+                setMsg("Server Error Occured");
+            }
+        });
+        
+        
+    }
+
+    return (
+        <div>
+            <LeftBar />
+            <div class="container">
+                <div class="content">
+                    <div class="content-3">
+                        <h1>CREATE A NEW SELLER</h1>
+                             
+                        <form action="" method="POST" onSubmit={handleForm}>
+
+                            <h1>{msg}</h1>
+                          
+                          <label>SELLER NAME</label><br />
+                          <input type="text" name="s_name" id="name" value={s_name} onChange={(e)=>{setName(e.target.value)}}/><span>{err.s_name? err.s_name[0]:''}</span><br/>
+                          
+                          <label>PHONE NUMBER</label><br/>
+                          <input type="text" name="s_phn" id="phn" value={s_phn} onChange={(e)=>{setPhn(e.target.value)}} /><span>{err.s_phn? err.s_pass[0]:''}</span><br/>
+                          
+                          <label>EMAIL</label><br/>
+                          <input type="text" name="s_mail" id="email" value={s_mail} onChange={(e)=>{setMail(e.target.value)}} /><span>{err.s_mail? err.s_mail[0]:''}</span><br/>
+                          
+                          <label>PASSWORD</label><br/>
+                          <input type="password" name="s_pass" id="pass" value={s_pass} onChange={(e)=>{setPass(e.target.value)}}/><span>{err.s_pass? err.s_pass[0]:''}</span><br/>
+                          
+                          <label>ADDRESS</label><br/>
+                          <input type="text" name="s_add" id="add" value={s_add} onChange={(e)=>{setAdd(e.target.value)}} /><span>{err.s_add? err.s_add[0]:''}</span><br/>
+                          
+                          <button>ADD</button>
+                        </form>
+                
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+
+}
+export default CreateSeller;
---
+++ src/Components/Admin/EditSeller.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import LeftBar from "./Bar/LeftBar";
+
+
+const EditSeller=(props)=> {
+
+    const [inputs,setInputs] = useState({});
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const[msg,setMsg] = useState("");
+    const[err,setErr] = useState("");
+
+
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/api/admin/files/showSeller/${id}`).then((res)=>{
+            setInputs({
+                s_id:res.data.s_id,
+                s_name:res.data.s_name,
+                s_phn:res.data.s_phn,
+                s_mail:res.data.s_mail,
+                s_pass:res.data.s_pass,
+                s_add:res.data.s_add,
+                
+            });
+        });
+    },[id]);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values,[name]:value}))
+        setMsg('');
+        setErr('');
+    }
+
+    const submitForm = (e) =>{
+        const thisClicked = e.currentTarget;
+        thisClicked.innerText = "Updating";
+        console.log(inputs);
+        axios.post(`http://localhost:8000/api/admin/files/showSeller`, inputs).then((rsp)=>{
+            navigate('/Admin/Seller');
+            setMsg(rsp.data.msg);
+        },(err)=>{
+            if(err.response.status==422) //for data validation
+            {
+                setErr(err.response.data);
+            }else{
+                setMsg("Server Error Occured");
+            }
+        });
+        
+    }
+
+
+    return (
+
+        <div>
+             <LeftBar />
+             <div class="container">
+                 <div class="content">
+                     <div class="content-3">
+                         <h1>UPDATE SELLER</h1>
+                         <h1>{msg}</h1>                           
+                           <label>SELLER ID</label><br/>
+                           <input readOnly name="s_id" id="id" value={inputs.s_id || '' } onChange={handleChange} /><br/>
+                           <label>SELLER NAME</label><br/>
+                           <input type="text" name="s_name" id="name" value={inputs.s_name || '' } onChange={handleChange} /><span>{err.s_name? err.s_name[0]:''}</span><br/>                        
+                           <label>PHONE NUMBER</label><br/>
+                           <input type="text" name="s_phn" id="phn" value={inputs.s_phn || '' } onChange={handleChange} /><span>{err.s_phn? err.s_phn[0]:''}</span><br/>                        
+                           <label>EMAIL</label><br/>
+                           <input type="text" name="s_mail" id="email" value={inputs.s_mail || '' } onChange={handleChange} /><span>{err.s_mail? err.s_mail[0]:''}</span><br/>                        
+                           <label>PASSWORD</label><br/>
+                           <input type="text" name="s_pass" id="pass" value={inputs.s_pass || '' } onChange={handleChange} /><span>{err.s_pass? err.s_pass[0]:''}</span><br/>                        
+                           <label>ADDRESS</label><br/>
+                           <input type="text" name="s_add" id="add" value={inputs.s_add || '' } onChange={handleChange} /><span>{err.s_add? err.s_add[0]:''}</span><br/>                        
+                           <button type="button" onClick={submitForm}>UPDATE</button>  
+                           <a href="/Admin/Seller">CANCEL</a>                                      
+                     </div>
+                 </div>
+             </div>
+         </div>
+    )
+}
+
+export default EditSeller;
