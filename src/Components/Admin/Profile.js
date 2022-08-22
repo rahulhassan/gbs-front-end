7ac7e2987e8c943e5758d6c13b079720c20d0381
SHADIK HASAN <57946665+SK-SHADIK@users.noreply.github.com>
2022-08-22 15:53:49 +0600

    Update Profile.js

--- src/Components/Admin/Profile.js
+++ src/Components/Admin/Profile.js
@@ -3,42 +3,6 @@ import EditAdmin from "./EditAdmin";
 import EditPic from "./EditPic";
 
 const Profile=()=>{
-
-
-    // const[mfile,setFile] = useState(null);
-    const [admin,setAdmin] = useState({});
-    const navigate = useNavigate();
-    const[imageses,setInputImage] = useState({});
-
-
-    useEffect(()=>{
-        axios.get("http://localhost:8000/api/admin/files/profile")
-        .then((rsp)=>{
-            setAdmin(rsp.data);
-            console.log(rsp);
-        },(err)=>{
-
-        }) 
-    },[]);
-
-    const handleForm = (e) =>{
-        e.preventDefault();
-        var fData = new FormData();
-        fData.append("file", imageses);
-
-        const thisClicked = e.currentTarget;
-        thisClicked.innerText = "Updating";
-
-        axios.post("http://localhost:8000/api/admin/files/upload", fData)
-        .then((rsp)=>{
-            navigate('/Admin/Profile');
-            thisClicked.innerText = "Update";
-            
-        })
-                
-    }
-
-
     return(
         <div>   
             <div class="ad-btn2"><a href="/Admin/Dashboard">GO BACK!</a></div>                             
